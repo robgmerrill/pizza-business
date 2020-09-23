2766daaa01320034d2db1515d6722d1670efe45b
Robert Merrill <robgmerrill@gmail.com>
2020-09-23 07:40:09 -0700

    Added different pages

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,12 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/robertmerrill/Desktop/pizza-slices/gatsby/.cache/dev-404-page.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/robertmerrill/Desktop/pizza-slices/gatsby/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/robertmerrill/Desktop/pizza-slices/gatsby/src/pages/404.js"))),
+  "component---src-pages-beers-js": hot(preferDefault(require("/Users/robertmerrill/Desktop/pizza-slices/gatsby/src/pages/beers.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/robertmerrill/Desktop/pizza-slices/gatsby/src/pages/index.js"))),
+  "component---src-pages-order-js": hot(preferDefault(require("/Users/robertmerrill/Desktop/pizza-slices/gatsby/src/pages/order.js"))),
+  "component---src-pages-pizzas-js": hot(preferDefault(require("/Users/robertmerrill/Desktop/pizza-slices/gatsby/src/pages/pizzas.js"))),
+  "component---src-pages-slicemasters-js": hot(preferDefault(require("/Users/robertmerrill/Desktop/pizza-slices/gatsby/src/pages/slicemasters.js")))
 }
 
