069d5be192698633f7e9d512bc4f425b84902d4f
Robert Merrill <robgmerrill@gmail.com>
2020-09-23 08:22:16 -0700

    Add global styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
 
 export default function Layout({ children }) {
   console.log(children);
   return (
     <div>
+      <GlobalStyles />
       <Nav />
       {children}
       <Footer />
